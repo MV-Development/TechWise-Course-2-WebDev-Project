9bc04a966266a24fd960af867cdfde8666b907be
Alpha <mikevisa4500x@gmail.com>
2023-01-25 13:17:48 -0500

    Made chandes to testing.js based on table.js file from blackjack project

--- public/testing.js
+++ public/testing.js
@@ -2,3 +2,18 @@ const example = 11;
 var TMDBToken = "46eb4bee79974ac4c17249b988d15fb8";
 const baseURL = "https://api.themoviedb.org/3";
 
+let movieID;
+
+function getMovie(callback) {
+    fetch(baseURL + `/mivie{movie_id}`)
+        .then((res) => res.json())
+        .then((data) => {
+            callback(data)
+        });
+}
+
+getMovie((data) => movieID = data.movie_id);
+
+function getConfiguration(api_key) {
+    https://api.themoviedb.org/3/configuration?api_key=<<api_key>>
+}
