60ae2f642ff78ef422f1783f73108639362718ef
Alpha <mikevisa4500x@gmail.com>
2023-01-26 13:12:23 -0500

    Added get requestToken function. I am still unable to assign the response values to variables. Will further study of the documentation.

--- public/testing.js
+++ public/testing.js
@@ -1,11 +1,10 @@
-const example = 11;
 var APIKey = '46eb4bee79974ac4c17249b988d15fb8';
 const baseURL = "https://api.themoviedb.org/3";
 
-let sessionToken;
 
-function getSessionToken(callback) {
-    return fetch(`${baseURL}/authentication/token/new?api_key=${APIKey}`)
+let requestToken;
+function getRequestToken(callback) {
+    fetch(`${baseURL}/authentication/token/new?api_key=${APIKey}`)
         .then((res) => res.json())
         .then((data) => {
             console.log(data);
@@ -13,8 +12,19 @@ function getSessionToken(callback) {
         });
 }
 
-getSessionToken((data) => (sessionToken = data.request_token));
+getRequestToken((data) => (requestToken = data.request_token));
+console.log(requestToken);
 
-console.log(sessionToken);
 
+let guestSessionToken;
+function getGuestSessionToken(callback) {
+    fetch(`${baseURL}/authentication/guest_session/new?api_key=${APIKey}`)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            callback(data);
+        });
+}
 
+getGuestSessionToken((data) => (guestSessionToken = data.guest_session_id));
+console.log(guestSessionToken);
