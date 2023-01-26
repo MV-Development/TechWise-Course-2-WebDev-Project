8323d0f263b0e837ae38472a42c12d9be29c98b1
Alpha <mikevisa4500x@gmail.com>
2023-01-25 19:04:20 -0500

    Successfully contacted api and got a response. I have yet to be able to impliment the response.

--- public/testing.js
+++ public/testing.js
@@ -1,31 +1,20 @@
 const example = 11;
-var APIKey = "46eb4bee79974ac4c17249b988d15fb8";
+var APIKey = '46eb4bee79974ac4c17249b988d15fb8';
 const baseURL = "https://api.themoviedb.org/3";
-
-
 
 let sessionToken;
 
-function getSessionToken(api_key) {
-    return `https://api.themoviedb.org/3/authentication/token/new?api_key=${APIKey}`
-}
-
-sessionToken = getSessionToken(APIkey);
-
-function getConfiguration(api_key) {
-    return `https://api.themoviedb.org/3/configuration?api_key=<<${api_key}>>`
-}
-
-let movieID;
-
-
-function getMovie(callback) {
-    fetch(baseURL + `/movie{movie_id}`)
+function getSessionToken(callback) {
+    return fetch(`${baseURL}/authentication/token/new?api_key=${APIKey}`)
         .then((res) => res.json())
         .then((data) => {
-            callback(data)
+            console.log(data);
+            callback(data);
         });
 }
 
-getMovie((data) => movieID = data.movie_id);
+getSessionToken((data) => (sessionToken = data.request_token));
+
+console.log(sessionToken);
+
 
