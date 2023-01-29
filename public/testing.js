6d00c7c6d168d60e686d123377ec136bc8010f25
Daniel Zombek <djzombek@gmail.com>
2023-01-29 12:28:54 -0500

    Added logo and modified CSS for better dimensions

--- public/testing.js
+++ public/testing.js
@@ -1,4 +1,11 @@
-const example = 11;
-var TMDBToken = "46eb4bee79974ac4c17249b988d15fb8";
+var APIKEY = "3ce534b5cb32c55ec594d1642b08ac7d";
 const baseURL = "https://api.themoviedb.org/3";
+let sessionToken;
 
+function getSessionToken(API_KEY) {
+    return `https://api.themoviedb.org/3/authentication/session/new?api_key=${APIKEY}`
+}
+
+sessionToken = getSessionToken(APIKEY);
+
+document.getElementById("session-token").innerHTML = sessionToken;
