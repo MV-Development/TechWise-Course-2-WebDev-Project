830641010b37a6e38a4086f43455d32fc0c56c5f
Alpha <mikevisa4500x@gmail.com>
2023-02-01 20:06:43 -0500

    I'd say that I have successfully produced an HTML page that generates info based on a given movie ID. It's not much, but it is a solid start.

--- public/movieDetails.js
+++ public/movieDetails.js
@@ -2,46 +2,84 @@ var APIKey = '46eb4bee79974ac4c17249b988d15fb8';
 const baseURL = "https://api.themoviedb.org/3";
 let sessionToken;
 
-
+/*
 let requestToken;
 function getRequestToken(callback) {
-    fetch(`${baseURL}/authentication/token/new?api_key=${APIKey}`)
+    return fetch(`${baseURL}/authentication/token/new?api_key=${APIKey}`)
         .then((res) => res.json())
         .then((data) => {
-            //console.log(data);
+//            console.log(data);
             callback(data);
         });
 }
 
-getRequestToken((data) => (requestToken = data.request_token));
-console.log(requestToken);
+getRequestToken((data) => {
+    (requestToken = data.request_token)
+    console.log(requestToken);
+});
+
 
 
 let guestSessionToken;
 function getGuestSessionToken(callback) {
-    fetch(`${baseURL}/authentication/guest_session/new?api_key=${APIKey}`)
+    return fetch(`${baseURL}/authentication/guest_session/new?api_key=${APIKey}`)
         .then((res) => res.json())
         .then((data) => {
-            //console.log(data);
+//            console.log(data);
             callback(data);
         });
 }
 
-getGuestSessionToken((data) => (guestSessionToken = data.guest_session_id));
-console.log(guestSessionToken);
+getGuestSessionToken((data) => {
+    guestSessionToken = data.guest_session_id
+    console.log(guestSessionToken);
+});
 
+
+
+*/
 
 
 const movieInfoSection = document.querySelector('#movieInfo');
-
 let movieInfo;
 function getMovieDetails(callback) {
-    fetch(`${baseURL}/search/movie?api_key=${APIKey}&query=Jack+Reacher`)
-        .then((res) => res.jason())
-        .then((data) => {
+    return fetch(`${baseURL}/movie/${movie_id}?api_key=${APIKey}&language=en-US`)
+        .then((res) => res.json())
+        .then((data) => 
             callback(data)
-        });
+        );}
+
+
+
+
+
+function clearDetails(domNode) {
+    while (domNode.firstChild) {
+        domNode.removeChild(domNode.firstChild);
+    }
 }
 
 
-getMovieDetails((data) => ())
+
+
+function populateMovieDetails(movie_id) {
+    getMovieDetails((data) => {
+    (movieInfo = data)
+        clearDetails(movieInfoSection);
+        var information = document.createElement("li");
+        information.innerText = "Title";
+        movieInfoSection.appendChild(information);
+        for (let info in movieInfo) {
+            information = document.createElement("li");
+//            console.log(movieInfo[info]);
+            information.innerText = movieInfo[info];
+            movieInfoSection.appendChild(information);
+        }
+    });
+};
+
+
+
+let movie_id = '667538';
+
+populateMovieDetails(movie_id);
